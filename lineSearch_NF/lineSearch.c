6fbb10416a7a5d233cc331053962317714ba9990
0verlo <Lyleoverlord26@gmail.com>
2017-02-27 07:20:11 -0800

    new file:   lineSearch_NF/.depend 	new file:   lineSearch_NF/Makefile 	new file:   lineSearch_NF/TARGET 	new file:   lineSearch_NF/lineSearch.c 	new file:   lineSearch_NF/lineSearch.o

---
+++ lineSearch_NF/lineSearch.c
@@ -0,0 +1,49 @@
+/*************************************************************************
+	> File Name: lineSearch.c
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 27 Feb 2017 06:18:43 AM PST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+
+int lineSearch_simple1(int key,int array[],int n)
+{
+	for(int i = 0;i < n;i++){
+		if(key == array[i]){
+			return i;
+		}
+	}
+	return -1;
+}
+
+void *lineSearch_NF(void *key,void *base,int n,int elemSize)
+{
+	for(int i = 0;i < n;i++){
+		void *elemAddr = (char*)base + i*elemSize;
+		//A little trick makes the memory addr calculation turns to simple math.
+		if(0 == memcmp(key,base,elemSize))
+			return elemAddr;
+	}
+	return NULL;
+}
+
+void main()
+{
+	int array[] = {1,95,23,3,5,78,2};
+	int key = 78;
+	int size = 7;
+	int searchResult = -1;
+
+	searchResult = lineSearch_simple1(key,array,size);
+
+	printf("The key numb %d is at the %dth play of the line.\n",key,searchResult);
+
+	float arrayF[] = {1.22,3.221,5.23,34.9,3.14,9.22};
+	float keyF = 3.14;
+	int n = 6;
+	float *fGet = NULL;
+	fGet = lineSearch_NF(&key,arrayF,n,sizeof(key));
+	printf("Got key %f in arrayF. Memory addr is",*fGet);
+}
