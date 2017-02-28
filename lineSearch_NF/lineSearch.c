f43243e9e1d6d504a263a1ce59c22cd542df9f46
0verlo <Lyleoverlord26@gmail.com>
2017-02-28 09:55:51 +0800

    modified:   Makefile 	modified:   TARGET 	modified:   lineSearch.c 	modified:   lineSearch.o

--- lineSearch_NF/lineSearch.c
+++ lineSearch_NF/lineSearch.c
@@ -18,37 +18,38 @@ int lineSearch_simple1(int key,int array[],int n)
 	return -1;
 }
 
-void *lineSearch_NF(void *key,void *base,int n,int elemSize)
+//void *lineSearch_NF(void *key,void *base,int n,int elemSize)
+int lineSearch_NF(void *key,void *base,int n,int elemSize)
 {
 	for(int i = 0;i < n;i++){
 		void *elemAddr = (char*)base + i*elemSize;
-		printf("--%d--\n",i);
 		//A little trick makes the memory addr calculation turns to simple math.
 		if(0 == memcmp(key,elemAddr,elemSize))
-			return elemAddr;
+			return i;
 	}
-	printf("---\n");
-	return NULL;
+	return -1;
 }
 
 void main()
 {
 	int array[] = {1,95,23,3,5,78,2};
 	int key = 78;
-	int size = 7;
+	int n = 7;
 	int searchResult = -1;
 
-	searchResult = lineSearch_simple1(key,array,size);
+	searchResult = lineSearch_simple1(key,array,n);
 
-	printf("The key numb %d is at the %dth play of the line.\n",key,searchResult);
+	printf("The key numb %d is at the %dth place of the line.\n",key,searchResult);
 
-	printf("-----------------------\n");
 	float arrayF[] = {1.22,3.221,5.23,34.9,3.14,9.22};
 	float keyF = 3.14;
-	int n = 6;
-	float *fGet = NULL;
-	printf("-----------------------\n");
-	fGet = lineSearch_NF(&key,arrayF,n,sizeof(key));
-	if(NULL != fGet)
-		printf("Got key %f in arrayF. Memory addr is",*fGet);
+	int nF = 6;
+	searchResult = lineSearch_NF(&keyF,arrayF,nF,sizeof(keyF));
+	printf("The float keyF %f is at the %dth place of the line.\n",keyF,searchResult);
+
+	double arrayLF[] = {1.22,3.221,5.23,34.9,3.14,9.22};
+	double keyLF = 3.14;
+	int nLF = 6;
+	searchResult = lineSearch_NF(&keyLF,arrayLF,nLF,sizeof(keyLF));
+	printf("The double keyLF %lf is at the %dth place of the line.\n",keyLF,searchResult);
 }
