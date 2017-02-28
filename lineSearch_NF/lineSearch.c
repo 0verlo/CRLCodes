90e3f8c4612de8520ec11c2159d663965a5b7518
0verlo <Lyleoverlord26@gmail.com>
2017-02-28 17:12:26 +0800

    modified:   TARGET 	modified:   lineSearch.c 	modified:   lineSearch.o

--- lineSearch_NF/lineSearch.c
+++ lineSearch_NF/lineSearch.c
@@ -17,7 +17,7 @@ int lineSearch_simple1(int key,int array[],int n)
 	}
 	return -1;
 }
-
+/*************************************************************************/
 //void *lineSearch_NF(void *key,void *base,int n,int elemSize)
 int lineSearch_NF(void *key,void *base,int n,int elemSize)
 {
@@ -29,27 +29,59 @@ int lineSearch_NF(void *key,void *base,int n,int elemSize)
 	}
 	return -1;
 }
+/*************************************************************************/
+int lineSearch_NF_Hook(void *key,void *base,int n,int elemSize,int (*cmpFN)(void *,void *))
+{
+	for(int i = 0;i < n;i++){
+		void *elemAddr = (char*)base + i*elemSize;
+		//A little trick makes the memory addr calculation turns to simple math.
+		if(0 == cmpFN(key,elemAddr))
+			return i;
+	}
+	return -1;
+}
+int intCmp(void *elem1,void *elem2)
+{
+	int *ip1 = elem1;
+	int *ip2 = elem2;
 
-void main()
+	return (*ip1 - *ip2);
+}
+
+/*************************************************************************
+ *test_zoon                                                              *
+ *************************************************************************/
+void test_Float()
+{
+	float arrayF[] = {1.22,3.221,5.23,34.9,3.14,9.22};
+	float keyF = 3.14;
+	int nF = 6;
+	int searchResult = -1;
+
+	printf("lineSearch_NF in %s:\n",__func__);
+	searchResult = lineSearch_NF(&keyF,arrayF,nF,sizeof(keyF));
+	printf("The float keyF %f is at the %dth place of the line.\n",keyF,searchResult);
+}
+
+void test_Int()
 {
 	int array[] = {1,95,23,3,5,78,2};
 	int key = 78;
 	int n = 7;
 	int searchResult = -1;
 
+	printf("lineSearch_simple1 in %s:\n",__func__);
 	searchResult = lineSearch_simple1(key,array,n);
-
 	printf("The key numb %d is at the %dth place of the line.\n",key,searchResult);
 
-	float arrayF[] = {1.22,3.221,5.23,34.9,3.14,9.22};
-	float keyF = 3.14;
-	int nF = 6;
-	searchResult = lineSearch_NF(&keyF,arrayF,nF,sizeof(keyF));
-	printf("The float keyF %f is at the %dth place of the line.\n",keyF,searchResult);
+	printf("llineSearch_NF_Hook in %s:\n",__func__);
+	searchResult = lineSearch_NF_Hook(&key,array,n,sizeof(key),intCmp);
+	printf("The key numb %d is at the %dth place of the line.\n",key,searchResult);
+}
+/*************************************************************************/
+void main()
+{
+	test_Int();
 
-	double arrayLF[] = {1.22,3.221,5.23,34.9,3.14,9.22};
-	double keyLF = 3.14;
-	int nLF = 6;
-	searchResult = lineSearch_NF(&keyLF,arrayLF,nLF,sizeof(keyLF));
-	printf("The double keyLF %lf is at the %dth place of the line.\n",keyLF,searchResult);
+	test_Float();
 }
